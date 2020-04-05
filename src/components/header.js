bf901f5283784c184ce3818cb11242148e2e4c16
eduarmreyes <eduar.mreyes@gmail.com>
2020-04-04 20:13:22 -0700

    Add childImageSharp on background image :raised_hands:

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,6 @@
-import { Link } from "gatsby"
+import { Link, graphql, StaticQuery } from "gatsby"
 import React from "react"
+import BackgroundImage from "gatsby-background-image"
 import Menu from "./menu"
 
 import ThemeContext from "../context/ThemeContext"
@@ -7,53 +8,79 @@ import ElSalvadorConectadoLogo from "../images/elsalvadorconectado-logo.svg"
 import ImagenManosEnMedio from "../images/perry-grone-lbLgFFlADrY-unsplash.jpg"
 
 const Header = () => (
-  <ThemeContext.Consumer>
-    {theme => (
-      <div>
-        <div
-          style={{
-            backgroundImage: `url(${ImagenManosEnMedio})`,
-            backgroundPosition: `center center`,
-            backgroundSize: `cover`,
-            backgroundColor: `rgba(23, 72, 237, 0.85)`,
-            marginBottom: `1.45rem`,
-            minHeight: `840px`,
-          }}
+  <StaticQuery
+    query={graphql`
+      query {
+        desktop: file(
+          relativePath: { eq: "perry-grone-lbLgFFlADrY-unsplash.jpg" }
+        ) {
+          childImageSharp {
+            fluid(quality: 90, maxWidth: 1920) {
+              ...GatsbyImageSharpFluid_withWebp
+            }
+          }
+        }
+      }
+    `}
+    render={data => {
+      // Set ImageData
+      const imageData = data.desktop.childImageSharp.fluid
+      return (
+        <BackgroundImage
+          Tag="section"
+          fluid={imageData}
+          backgroundColor={`#1748ED`}
         >
-          <div
-            style={{
-              margin: `0 auto`,
-              maxWidth: 960,
-              padding: `1.45rem 1.0875rem`,
-            }}
-          >
-            <Link to="/">
-              <img
-                src={ElSalvadorConectadoLogo}
-                alt="El texto se lee El Salvador Conectado con un logotipo formando una especie de abrazo"
-              />
-            </Link>
+          <ThemeContext.Consumer>
+            {theme => (
+              <div>
+                <div
+                  style={{
+                    marginBottom: `1.45rem`,
+                    minHeight: `840px`,
+                  }}
+                >
+                  <div
+                    style={{
+                      margin: `0 auto`,
+                      maxWidth: 960,
+                      padding: `1.45rem 1.0875rem`,
+                    }}
+                  >
+                    <Link to="/">
+                      <img
+                        src={ElSalvadorConectadoLogo}
+                        alt="El texto se lee El Salvador Conectado con un logotipo formando una especie de abrazo"
+                      />
+                    </Link>
 
-            <Menu />
+                    <Menu />
 
-            <button className="dark-switcher" onClick={theme.toggleDark}>
-              {theme.dark ? <span>☀</span> : <span>☾</span>}
-            </button>
-          </div>
-        </div>
-        <span>
-          Photo by{" "}
-          <a href="https://unsplash.com/@perrygrone?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">
-            Perry Grone
-          </a>{" "}
-          on{" "}
-          <a href="/s/photos/together?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">
-            Unsplash
-          </a>
-        </span>
-      </div>
-    )}
-  </ThemeContext.Consumer>
+                    <button
+                      className="dark-switcher"
+                      onClick={theme.toggleDark}
+                    >
+                      {theme.dark ? <span>☀</span> : <span>☾</span>}
+                    </button>
+                  </div>
+                </div>
+                <span>
+                  Photo by{" "}
+                  <a href="https://unsplash.com/@perrygrone?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">
+                    Perry Grone
+                  </a>{" "}
+                  on{" "}
+                  <a href="/s/photos/together?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">
+                    Unsplash
+                  </a>
+                </span>
+              </div>
+            )}
+          </ThemeContext.Consumer>
+        </BackgroundImage>
+      )
+    }}
+  ></StaticQuery>
 )
 
 export default Header
