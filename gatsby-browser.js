8ae2f3a6ca88cb9ad2a62c32c46ccb06f3bf210c
moudev <rigobertomauriciomartinez@gmail.com>
2020-04-06 17:04:25 -0600

    Add TailwindCSS package

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,7 @@
 import React from "react"
 import { ThemeProvider } from "./src/context/ThemeContext"
+import "tailwindcss/dist/base.css"
+
 export const wrapRootElement = ({ element }) => (
   <ThemeProvider>{element}</ThemeProvider>
 )
