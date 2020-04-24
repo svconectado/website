db3a37fa03299b82020601e34a40c968c1c7d8d8
moudev <rigobertomauriciomartinez@gmail.com>
2020-04-24 09:32:35 -0600

    Update blog and page templates, one file for each type
      - Adding page-template with the difference that this template
        doesn't show the title and author information

--- gatsby-node.js
+++ gatsby-node.js
@@ -34,7 +34,7 @@ exports.createPages = async ({ graphql, actions }) => {
     postResults.data.wpgraphql.posts.edges.forEach(({ node }) => {
       createPage({
         path: node.slug,
-        component: path.resolve("./src/templates/blog-page-template.js"),
+        component: path.resolve("./src/templates/blog-template.js"),
         context: {
           // This is the $slug variable
           // passed to blog-post.js
@@ -75,7 +75,7 @@ exports.createPages = async ({ graphql, actions }) => {
     pageResults.data.wpgraphql.pages.edges.forEach(({ node }) => {
       createPage({
         path: node.slug,
-        component: path.resolve("./src/templates/blog-page-template.js"),
+        component: path.resolve("./src/templates/page-template.js"),
         context: {
           // This is the $slug variable
           // passed to blog-post.js
